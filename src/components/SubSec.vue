84eb08cda3eb0f3dbd6c33d22fd5464e62ad49cf
iluxaBoy <illapetefi@gmail.com>
2023-07-26 17:05:58 +0300

    EV-7

--- src/components/SubSec.vue
+++ src/components/SubSec.vue
@@ -1,27 +1,15 @@
 <template>
     <div class="container">
-        <v-card class="rounded-xl py-7">
-            <v-row class="top-sec mx-auto d-flex justify-space-between">
-                <v-col class="pa-0" cols="auto">
-                    <v-card-subtitle class="pa-0 mb-3">Тип:</v-card-subtitle>
-                    <v-card-text class="pa-0 font-weight-medium">Подписка</v-card-text>
-                </v-col>
-                <v-col class="pa-0" cols="auto">
-                    <v-card-subtitle class="pa-0 mb-3">Распределение:</v-card-subtitle>
-                    <v-card-text class="pa-0 font-weight-medium">50 000 000, 000 EDU</v-card-text>
-                </v-col>
-                <v-col class="pa-0" cols="auto">
-                    <v-card-subtitle class="pa-0 mb-3">Цена:</v-card-subtitle>
-                    <v-card-text class="pa-0 font-weight-medium">1 EDU = 0.00015281BNB</v-card-text>
-                </v-col>
-                <v-col class="pa-0" cols="auto">
-                    <v-card-subtitle class="pa-0 mb-3">Мин. вложения:</v-card-subtitle>
-                    <v-card-text class="pa-0 font-weight-medium">0.1 BNB</v-card-text>
+        <v-card class="rounded-xl py-md-7 px-md-7 px-4 py-6">
+            <v-row class="top-sec mx-auto d-flex flex-md-row flex-column align-start align-md-center justify-space-between">
+                <v-col v-for="id in data" :key="id" class="pa-0 mb-3" cols="auto">
+                    <v-card-subtitle class="pa-0 mb-1">{{ id.name + ":" }}</v-card-subtitle>
+                    <span class="pa-0 text-subtitle-1 font-weight-medium">{{ id.text }}</span>
                 </v-col>
             </v-row>
             <div class="bott-sec">
                 <div class="rounded mx-auto mt-7">
-                    <v-card-subtitle class="pa-0 text-center">Макс. на пользователя</v-card-subtitle>
+                    <v-card-subtitle class="pa-0 text-center">Max. per user</v-card-subtitle>
                     <v-card-text class="pa-0 font-weight-medium text-center">300000 EDU = 45,8429 BNB (=14294
                         USD)</v-card-text>
                 </div>
@@ -31,45 +19,47 @@
 </template>
 
 <script setup>
-import { useDisplay } from 'vuetify';
+import { ref } from 'vue';
 
-const width = useDisplay();
-
-function isMobile() {
-    return width.value <= 750;
-}
+const data = ref([
+    {
+        id: 1,
+        name: "Type",
+        text: "Subscription"
+    },
+    {
+        id: 2,
+        name: "Distribution",
+        text: "50 000 000, 000 EDU"
+    },
+    {
+        id: 3,
+        name: "Price",
+        text: "1 EDU = 0.00015281BNB"
+    },
+    {
+        id: 3,
+        name: "Min. attachments",
+        text: "0.1 BNB"
+    }
+]);
 </script>
 
-<style scoped>
-.v-card {
-    padding: 0 40px;
-}
-
-.v-row {
-    font-size: 18px;
-}
-
-.bott-sec div {
-    padding: 8px 144px;
-    max-width: 669px;
-    background-color: #DFDFDF;
-    font-size: 18px;
+<style scoped lang="scss">
+.bott-sec {
+    div {
+        padding: 8px 144px;
+        max-width: 669px;
+        background-color: #DFDFDF;
+        font-size: 18px;
+    }
 }
 
 @media (max-width: 900px) {
-    .v-card {
-        padding: 0 12px;
-    }
 
-    .top-sec {
-        flex-direction: column;
-        align-content: center;
-        font-size: 16px;
-    }
-
-    .v-col {
-        margin-top: 26px;
-    }
+    // .v-col {
+    //     margin-top: 26px;
+    // }
 
     .v-col:first-of-type {
         margin-top: 0;
